0537fede722b46dce362315a7282aef2ccb7db0c
Humberto Angelucci <angeluccirincon@gmail.com>
2021-07-24 11:07:31 +0200

    update

--- src/pages/MyPlants.jsx
+++ src/pages/MyPlants.jsx
@@ -13,26 +13,6 @@ function MyPlants(props) {
     }
     return (
         <div>
-<<<<<<< HEAD
-            Hello I'm all your plants
-            <p> Login status: {isLoggedIn.toString()}</p>
-            <Link to='/plants/create'>Add Plant</Link>
-            {
-                plants.map((plant, i) => {
-                    return(
-                     <div key={i}>
-                        <p>
-                        <Link to={`/plants/${plant._id}`}>{plant.name}</Link>
-                        </p>
-                     </div>
-                    
-             
-
-                        
-                    )
-                })
-            }
-=======
             <SideNav onLogOut={onLogOut} />
             <div className="body-container">
                 Hello I'm all your plants
@@ -50,7 +30,6 @@ function MyPlants(props) {
                     })
                 }
             </div>
->>>>>>> ff6e5db169b0be4d681bf99cea16407e5ea6ddf8
         </div>
     );
 };
