64f9e1822504c7456ee6211aa61ef548856715dd
elyzx <ellenchildren@icloud.com>
2021-07-26 14:32:35 +0200

    changed plantdetail default state value

--- src/components/EditPlant.jsx
+++ src/components/EditPlant.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
+
+// Material UI
 import Container from '@material-ui/core/Container';
 
 function EditPlant(props) {
     const {isLoggedIn} = props
-    const [plantDetail, updatePlantDetail] = useState(null)
+    const [plantDetail, updatePlantDetail] = useState([])
 
     useEffect(async () => {
         try {
